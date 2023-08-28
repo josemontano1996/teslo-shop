f0a6e80efb0cf38cdac2a450a35fd69c347370c2
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-08-28 15:07:52 +0200

    implementing swr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,22 @@ import '@fontsource/roboto/700.css';
 
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { lightTheme } from '@/themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline/>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
+      }}
+    >
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,11 @@ import { NextPage } from 'next';
 import { Typography } from '@mui/material';
 
 import { ProductList, ShopLayout } from '@/components';
-import { initialData } from '@/database/products';
+import { useProducts } from '@/hooks';
 
-const Home: NextPage = () => {
+const HomePage: NextPage = () => {
+  const { products, isLoading } = useProducts('/products');
+
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Find the best Teslo products here'}>
       <Typography variant='h1' component='h1'>
@@ -13,11 +15,10 @@ const Home: NextPage = () => {
       <Typography variant='h2' sx={{ mb: 1 }}>
         All products
       </Typography>
-     {/*  TODO change as any */}
-      <ProductList products={initialData.products as any} />
+      {isLoading ? <h1>Is charging</h1> : <ProductList products={products} />}
     </ShopLayout>
   );
 };
 
-export default Home;
+export default HomePage;
 
